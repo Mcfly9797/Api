e159eab87d37f5e98833641e4f16c1dd3c462092
Nahuel Villarreal <55802619+NahuelVillarreal@users.noreply.github.com>
2020-08-24 04:56:53 -0300

    prueba
    prueba

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class SpeakersController : ControllerBase
     {
